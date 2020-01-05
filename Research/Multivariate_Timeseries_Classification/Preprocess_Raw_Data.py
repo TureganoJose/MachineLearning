a1e8d19b51126a75a46a7849ca3e3c71703308af
Jose Martinez <turegano.jose@gmail.com>
2020-01-05 19:31:21 +0000

    Added new CNN example for timeseries (translating from tf1 to tf2)

---
+++ Research/Multivariate_Timeseries_Classification/Preprocess_Raw_Data.py
@@ -0,0 +1,90 @@
+
+#Implementation of a couple of papers:
+#https://arxiv.org/pdf/1905.01697.pdf
+#http://aqibsaeed.github.io/2016-11-04-human-activity-recognition-cnn/
+#
+#Data here:
+#http://www.cis.fordham.edu/wisdm/dataset.php
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import tensorflow as tf
+
+#
+# % matplotlib inline
+#
+plt.style.use('ggplot')
+
+
+def read_data(file_path):
+    column_names = ['user-id', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
+    data = pd.read_csv(file_path, header=None, names=column_names)
+    return data
+
+
+def feature_normalize(dataset):
+    mu = np.mean(dataset, axis=0)
+    sigma = np.std(dataset, axis=0)
+    return (dataset - mu) / sigma
+
+
+def plot_axis(ax, x, y, title):
+    ax.plot(x, y)
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+
+def plot_activity(activity, data):
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 10), sharex=True)
+    plot_axis(ax0, data['timestamp'], data['x-axis'], 'x-axis')
+    plot_axis(ax1, data['timestamp'], data['y-axis'], 'y-axis')
+    plot_axis(ax2, data['timestamp'], data['z-axis'], 'z-axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+def windows(data, size):
+    start = 0
+    while start < data.count():
+        yield int(start), int(start + size)
+        start += (size / 2)
+
+
+def segment_signal(data, window_size=90):
+    segments = np.empty((0, window_size, 3))
+    labels = np.empty((0))
+    for (start, end) in windows(data["timestamp"], window_size):
+        x = data["x-axis"][start:end]
+        y = data["y-axis"][start:end]
+        z = data["z-axis"][start:end]
+        if (len(dataset["timestamp"][start:end]) == window_size):
+            segments = np.vstack([segments, np.dstack([x, y, z])])
+            labels = np.append(labels, stats.mode(data["activity"][start:end])[0][0])
+    return segments, labels
+
+# Preprocessing
+
+dataset = read_data('WISDM_ar_v1.1_raw.txt')
+dataset.dropna(axis=0, how='any', inplace= True)
+dataset['z-axis'].replace({';': ''}, regex=True, inplace=True)
+dataset['z-axis']=dataset['z-axis'].astype(float)
+
+dataset['x-axis'] = feature_normalize(dataset['x-axis'])
+dataset['y-axis'] = feature_normalize(dataset['y-axis'])
+dataset['z-axis'] = feature_normalize(dataset['z-axis'])
+
+#for activity in np.unique(dataset["activity"]):
+#    subset = dataset[dataset["activity"] == activity][:180]
+#    plot_activity(activity,subset)
+
+segments, labels = segment_signal(dataset)
+labels = np.asarray(pd.get_dummies(labels), dtype = np.int8)
+reshaped_segments = segments.reshape(len(segments), 1,90, 3)
+np.save('labels', labels)
+np.save('segments', segments)
